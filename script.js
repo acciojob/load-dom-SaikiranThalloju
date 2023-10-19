2a6a524b3a1218b638c76b981b4d3c763f09ec37
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-10-19 10:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 const load = document.getElementByTag("body")
-load.addEventListener("DOMonload",()=>{
+load.addEventListener("onload",()=>{
 	load.innerText="DOM load success"
 })
